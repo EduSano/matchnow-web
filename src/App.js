ba5d5498227739d48c5a208f1b016b4c3c5d8eba
EduSano <edusano@runnercafe.com.br>
2025-04-15 16:25:37 -0300

    Landing page do MatchNow!

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: 50}}>
+      <h1>MatchNow!</h1>
+      <p>MVP em construção - nova versão a caminho!</p>
+      <button>Começar agora</button>
+    </div>
+  );
+}
+export default App;
